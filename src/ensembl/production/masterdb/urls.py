6f534f55874e35aefda54f807e3d797c092cd7f1
Marc Chakiachvili <mchakiachvili@ebi.ac.uk>
2021-05-15 01:14:53 +0100

    Fixes url scheme for master db api rules.

--- src/ensembl/production/masterdb/urls.py
+++ src/ensembl/production/masterdb/urls.py
@@ -9,10 +9,3 @@
 #   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 #   See the License for the specific language governing permissions and
 #   limitations under the License.
-
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path(f'api/', include('ensembl.production.masterdb.api.urls')),
-]
